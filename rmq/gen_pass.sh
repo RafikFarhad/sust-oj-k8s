665fa6bbbb9d3383e4c4f322e5683b2ebfcb2738
RafikFarhad <rafikfarhad@gmail.com>
2020-03-16 02:20:37 +0600

    Initial commit - redis/rmq skeleton

---
+++ rmq/gen_pass.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+function get_byte()
+{
+    local BYTE=$(head -c 1 /dev/random | tr -d '\0')
+
+    if [ -z "$BYTE" ]; then
+        BYTE=$(get_byte)
+    fi
+
+    echo "$BYTE"
+}
+
+function encode_password()
+{
+    BYTE1=$(get_byte)
+    BYTE2=$(get_byte)
+    BYTE3=$(get_byte)
+    BYTE4=$(get_byte)
+
+    SALT="${BYTE1}${BYTE2}${BYTE3}${BYTE4}"
+    PASS="$SALT$1"
+    TEMP=$(echo -n "$PASS" | openssl sha256 -binary)
+    PASS="$SALT$TEMP"
+    PASS=$(echo -n "$PASS" | base64)
+    echo "$PASS"
+}
+
+encode_password $1
